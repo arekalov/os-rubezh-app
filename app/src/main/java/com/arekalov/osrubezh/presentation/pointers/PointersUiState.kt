17c4eb769d2ac038d2555681d8cba24d1dbb9c39
arekalov <artyom.rekalov@gmail.com>
2026-01-12 18:20:15 +0300

    [fix] remove hardcoded data

--- app/src/main/java/com/arekalov/osrubezh/presentation/pointers/PointersUiState.kt
+++ app/src/main/java/com/arekalov/osrubezh/presentation/pointers/PointersUiState.kt
@@ -3,10 +3,10 @@ package com.arekalov.osrubezh.presentation.pointers
 import com.arekalov.osrubezh.domain.model.PointersResult
 
 data class PointersUiState(
-    val directPointers: String = "16",
-    val pointerSizeBits: String = "32",
-    val blockSizeBytes: String = "512",
-    val fileSizeBytes: String = "235477247",
+    val directPointers: String = "",
+    val pointerSizeBits: String = "",
+    val blockSizeBytes: String = "",
+    val fileSizeBytes: String = "",
     val result: PointersResult? = null,
     val isLoading: Boolean = false,
     val error: String? = null,
